e197c63e87797f3baa562548c8aaa3e5f9fa1e3c
kas030 <kas_1120@163.com>
2026-07-14 22:11:09 +0800

    feat(cpu): 实现 zip 指令

--- rtl/datapath/alu.sv
+++ rtl/datapath/alu.sv
@@ -23,12 +23,15 @@ module alu #(
 );
     logic m_add , m_sub , m_and , m_or  , m_xor , m_sll , m_srl;
     logic m_sra , m_beq , m_bne , m_blt , m_bge , m_bgeu, m_bltu;
+    logic m_zip;
     logic [DATAWIDTH-1:0] add_lhs, add_rhs;
     logic                 cin, cout;
     logic [DATAWIDTH-1:0] r_addsub, r_and, r_or, r_xor;
     logic [DATAWIDTH-1:0] r_sll, r_srl, r_sra;
     logic [DATAWIDTH-1:0] r_slt, r_sltu;
+    logic [DATAWIDTH-1:0] r_zip;
     logic                 cmp_eq, cmp_lt, cmp_ltu;
+    integer               zip_i;
 
     assign m_add  = ALUControl[ 0];
     assign m_sub  = ALUControl[ 1];
@@ -44,6 +47,7 @@ module alu #(
     assign m_bge  = ALUControl[11];
     assign m_bgeu = ALUControl[12];
     assign m_bltu = ALUControl[13];
+    assign m_zip = ALUControl[22];
 
     assign add_lhs = A;
     assign add_rhs = (m_sub | m_blt | m_bge | m_bgeu | m_bltu) ? ~B : B;
@@ -59,6 +63,14 @@ module alu #(
     assign r_sll  = A << B[4:0];
     assign r_srl  = A >> B[4:0];
     assign r_sra  = ($signed(A)) >>> B[4:0];
+
+    always_comb begin
+        r_zip = '0;
+        for (zip_i = 0; zip_i < 16; zip_i = zip_i + 1) begin
+            r_zip[2*zip_i] = A[zip_i];
+            r_zip[2*zip_i+1] = A[zip_i+16];
+        end
+    end
 
     assign cmp_eq  = A == B;
     assign cmp_lt  = (A[31] &  ~B[31]) | ((~A[31] ^ B[31]) & r_addsub[31]);
@@ -81,5 +93,6 @@ module alu #(
                     {DATAWIDTH{m_srl        }} & r_srl    |
                     {DATAWIDTH{m_sra        }} & r_sra    |
                     {DATAWIDTH{m_blt        }} & r_slt    |
-                    {DATAWIDTH{m_bltu       }} & r_sltu;
+                    {DATAWIDTH{m_bltu       }} & r_sltu |
+                    {DATAWIDTH{m_zip}} & r_zip;
 endmodule
